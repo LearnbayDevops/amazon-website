95ecf12c1728157f4b653a8000d953c9c8f033bb
LearnbayDevops <learnbaydevops@gmail.com>
2024-08-25 12:43:23 +0530

    Added User Mgt Python File

---
+++ user_mgt.py
@@ -0,0 +1,8 @@
+def add_user(username):
+         print(f"User {username} added successfully!")
+
+def delete_user(username):
+ print(f"User {username} deleted successfully!")
+
+add_user("john_doe")
+delete_user("john_doe")
